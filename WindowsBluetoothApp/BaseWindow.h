5bd0b50e4624a789f7891a38cfbde22981c48124
Jonathan Chew <jchew513@gmail.com>
2019-01-30 16:08:28 -0800

    basic direct2d test

---
+++ WindowsBluetoothApp/BaseWindow.h
@@ -0,0 +1,62 @@
+#pragma once
+
+template <class T>
+class BaseWindow
+{
+public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		T *pThis = NULL;
+		if (uMsg == WM_NCCREATE)
+		{
+			CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+			pThis = (T*)pCreate->lpCreateParams;
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
+
+			pThis->m_hwnd = hwnd;
+		}
+		else
+		{
+			pThis = (T*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+		}
+		if (pThis)
+		{
+			return pThis->HandleMessage(uMsg, wParam, lParam);
+		}
+		else
+		{
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	BaseWindow() : m_hwnd(NULL) {}
+	BOOL Create(
+		PCWSTR lpWindowName,
+		DWORD dwStyle,
+		DWORD dwExStyle = 0,
+		int x = CW_USEDEFAULT,
+		int y = CW_USEDEFAULT,
+		int nWidth = CW_USEDEFAULT,
+		int nHeight = CW_USEDEFAULT,
+		HWND hWndParent = 0,
+		HMENU hMenu = 0
+	) {
+		WNDCLASS wc = { 0 };
+		wc.lpfnWndProc = T::WindowProc;
+		wc.hInstance = GetModuleHandle(NULL);
+		wc.lpszClassName = ClassName();
+
+		RegisterClass(&wc);
+
+		m_hwnd = CreateWindowEx(
+			dwExStyle, ClassName(), lpWindowName, dwStyle, x, y, nWidth, nHeight,
+			hWndParent, hMenu, GetModuleHandle(NULL), this
+		);
+
+		return (m_hwnd ? TRUE : FALSE);
+	}
+	HWND Window() const { return m_hwnd; }
+protected:
+	virtual PCWSTR ClassName() const = 0;
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+	HWND m_hwnd;
+};
--- WindowsBluetoothApp/Main.cpp
+++ WindowsBluetoothApp/Main.cpp
@@ -3,54 +3,21 @@
 #endif
 
 #include <Windows.h>
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+#include "MainWindow.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-	WNDCLASS wc = {};
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-
-	RegisterClass(&wc);
-
-	HWND hwnd = CreateWindowEx(
-		0,
-		CLASS_NAME,
-		L"Learn to Program Windows",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
-	if (hwnd == NULL) {
+	MainWindow win;
+	if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
+	{
 		return 0;
 	}
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(win.Window(), nCmdShow);
+
 	MSG msg = {};
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 	return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	switch (uMsg) {
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	case WM_PAINT: {
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-		EndPaint(hwnd, &ps);
-	}
-				   return 0;
-	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
